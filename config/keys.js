2347d75327e69b894761155f3b20cd67d6ad56b7
shivam <shivamjha1315@gmail.com>
2020-07-12 21:53:28 +0530

    MIDAS

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb+srv://shivam1315:'+ encodeURIComponent('shivam1315') + '@cluster0.y8hho.mongodb.net/test?retryWrites=true&w=majority';
+
+module.exports = {
+    mongoURI: dbPassword
+};
